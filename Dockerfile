21c50821a0ed390ad41ed4f21c1694a84040c5c9
lanjianghao <528601933@qq.com>
2024-06-05 12:28:16 +0000

    update dockerfile

--- Dockerfile
+++ Dockerfile
@@ -4,11 +4,12 @@ FROM pytorch/pytorch:1.9.1-cuda11.1-cudnn8-runtime
 # WORKDIR /temp
 
 # RUN apt-get update && apt-get install ffmpeg libsm6 libxext6 -y
-RUN apt-get update -y && apt-get install -y ffmpeg
+RUN apt-get update -y && apt-get install -y ffmpeg && apt-get clean
 
 RUN pip install -i https://pypi.tuna.tsinghua.edu.cn/simple --upgrade pip \
 && pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple \
-&& pip install numpy scipy scikit-image tqdm matplotlib opencv-python-headless yt-dlp
+&& pip install numpy scipy scikit-image tqdm matplotlib opencv-python-headless yt-dlp \
+&& pip cache purge
 
 WORKDIR /app
 
--- convert_dataset.py
+++ convert_dataset.py
@@ -18,36 +18,9 @@ def gen_src_data(root, data_list_path):
         data_path = line.split()[0]
         id, vid = data_path.split('/')
         yield id, vid, os.path.join(root, data_path)
-    # result_dict = {}
-    # with open(data_list_path) as f:
-    #     for line in f.readlines():
-    #         vid, result_desp = line.strip().split(' ', 1)
-
-    #         assert result_desp in _possible_result_desp
-    #         if len(vid.split('_')) == 3:
-    #             assert vid not in result_dict
-
-    #         result_dict[vid] = result_desp
-
-    # for id in os.listdir(root):
-    #     id_path = os.path.join(root, id)
-    #     if not os.path.isdir(id_path):
-    #         continue
-
-    #     for vid in os.listdir(id_path):
-    #         assert vid in result_dict
-    #         if result_dict[vid] == 'Success':
-    #             data_path = os.path.join(id_path, vid)
-    #             assert os.path.isdir(data_path)
-    #             assert os.path.isfile(os.path.join(data_path, 'video.mp4'))
-    #             assert os.path.isfile(os.path.join(data_path, 'audio.wav'))
-    #             assert os.path.isfile(os.path.join(data_path, 'landmarks.json'))
-    #             assert os.path.isfile(os.path.join(data_path, 'rois.json'))
-    #             assert os.path.isfile(os.path.join(data_path, 'ori_video.mp4'))
-    #             yield id, vid, data_path
 
 
-def convert_video_to_images(src_video_path, dst_dir, resize=None):
+def convert_video_to_images(src_video_path, dst_dir, resize=None, format='.jpg'):
     # cmd = ['ffmpeg', '-i', src_video_path, os.path.join(dst_dir, r'%d.png')]   # 使用png无损图片格式
     # try:
     #     subprocess.run(cmd, check=True, text=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
@@ -72,14 +45,14 @@ def convert_video_to_images(src_video_path, dst_dir, resize=None):
             except Exception:
                 return None
 
-        output_path = os.path.join(dst_dir, f"{frame_count}.png")
+        output_path = os.path.join(dst_dir, f"{frame_count}{format}")
         cv2.imwrite(output_path, frame)
         frame_count += 1
     # 释放视频对象
     cap.release()
 
 
-def convert_one(data_path, to_path, cp_rois_and_lms=False, resize=None):
+def convert_one(data_path, to_path, cp_rois_and_lms=False, resize=None, format='.jpg'):
     if os.path.exists(to_path):
         shutil.rmtree(to_path)
     os.makedirs(to_path)
@@ -89,14 +62,14 @@ def convert_one(data_path, to_path, cp_rois_and_lms=False, resize=None):
     landmarks_path = os.path.join(data_path, 'landmarks.json')
     rois_path = os.path.join(data_path, 'rois.json')
     
-    convert_video_to_images(video_path, to_path, resize)
+    convert_video_to_images(video_path, to_path, resize, format)
     shutil.copyfile(audio_path, os.path.join(to_path, 'audio.wav'))
     if cp_rois_and_lms:
         shutil.copyfile(landmarks_path, os.path.join(to_path, 'landmarks.json'))
         shutil.copyfile(rois_path, os.path.join(to_path, 'rois.json'))
 
 
-def convert_dataset(dataset_root, output_root, data_list_path, cp_rois_and_lms=False, max_workers=8, resize=None):
+def convert_dataset(dataset_root, output_root, data_list_path, cp_rois_and_lms=False, max_workers=8, resize=None, format='.jpg'):
     src_datas = list(gen_src_data(dataset_root, data_list_path))
     id_num = len(set(map(lambda x: x[0], src_datas)))
     
@@ -112,7 +85,7 @@ def convert_dataset(dataset_root, output_root, data_list_path, cp_rois_and_lms=F
         for id, vid, data_path in src_datas:
             dst_dir = os.path.join(output_root, id, vid)
 
-            f = executor.submit(convert_one, data_path, dst_dir, cp_rois_and_lms, resize)
+            f = executor.submit(convert_one, data_path, dst_dir, cp_rois_and_lms, resize, format)
             # convert_one(data_path, dst_dir, cp_rois_and_lms)
             futures.append(f)
         
@@ -136,6 +109,7 @@ def main():
     parser.add_argument('--copy_rois_and_lms', action='store_true', help='copy rois and landmarks to the target dataset.')
     parser.add_argument('--max_workers', type=int, help='max num of workers of process pool')
     parser.add_argument('--resize', type=int, default=None, help='resize each image to specific size')
+    parser.add_argument('--format', type=str, default='.jpg', help='Output image format, like \'.jpg\', \'.png\'')
     args = parser.parse_args()
     
     dataset_root = args.dataset_root
@@ -144,8 +118,9 @@ def main():
     cp_rois_and_lms = args.copy_rois_and_lms
     max_workers = args.max_workers
     resize = args.resize
+    format = args.format
     
-    convert_dataset(dataset_root, output_root, data_list_path, cp_rois_and_lms, max_workers=max_workers, resize=resize)
+    convert_dataset(dataset_root, output_root, data_list_path, cp_rois_and_lms, max_workers=max_workers, resize=resize, format=format)
 
 
 if __name__ == '__main__':
